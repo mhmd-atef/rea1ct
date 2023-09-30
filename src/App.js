f99c3c2bcd7242bc822ec5bcfdf61c99d15630a3
John Doe <johndoe@example.com>
2023-09-30 20:04:04 +0200

    Create Navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React  from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Layers from "./components/Layers/Layers";
+import "./App.css";
+
+
+let router = createBrowserRouter([
+  { path: '/', element: <Layers />, children: [
+    {index:true , element:<Home/>},
+    {path:'About' , element:<About/>},
+    {path:'Layers' , element:<Layers/>},
+    {path:'*' , element:<div className="  container-fluid   bg-danger">
+    <div className="row justify-content-center  vh-100">
+      <div className=" text-black col-md-6    text-center m-auto  fw-bolder"><h1>Not Found <span  className="text-dark align-content-cetner text-center m-auto col-md-2 d-flex">4 0 4</span> </h1></div>
+    </div>
+    </div>},
+
+  ] }
+])
+
+
+
+
+
+
+
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router}></RouterProvider>
 }
 
 export default App;
---
+++ src/components/About/About.jsx
@@ -0,0 +1,12 @@
+import React  from 'react'
+
+export default function About(){
+    return <>
+    <div>  
+    <h2> hello ffkdvk</h2>
+    <h2> hello ffkdvk</h2>
+    <h2> hello ffkdvk</h2>
+    <h2 className='text-bg-danger fa-arrow-right-to-bracket'> hellooo ffkdvk</h2>
+    </div>
+    </>
+  }
